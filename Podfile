4c902bdeb833ddca893b94d41eb5b70b40a3912d
Thomas Le <thomasle43@gmail.com>
2018-09-25 19:30:43 -0500

    flix part 1 tableview, network request, pull to refresh, and activity indicator

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flix_movieApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flix_movieApp
+	pod 'AlamofireImage', '~> 3.4'
+
+end
---
+++ flix_movieApp/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix_movieApp
+//
+//  Created by Thomas Le on 9/25/18.
+//  Copyright © 2018 Thomas Le. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
